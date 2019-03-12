9a767422e48cf1d605e026837056f05c24383e93
alex chalyy <alexchalyy@gmail.com>
2019-03-11 21:35:18 -0400

    modal pop-up window added for empty string input validation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,6 @@
 /*  This script utilizes api get calls to get dog breed information by search result and pull up local pet shelter results. */
 
 var breed;  //  This holds the dog breed name.
-var empty_string_validation_message = "<div class=\"modal\" tabindex=\"-1\" role=\"dialog\"><div class=\"modal-dialog\" role=\"document\"><div class=\"modal-content\"><div class=\"modal-header\"><h5 class=\"modal-title\">Woof woof!</h5><button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"false\">&times;</span></button></div><div class=\"modal-body\"><p>Please enter valid dog breed.</p></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button></div></div></div></div>";
 
 //----------------------------------------------------------------------------------------------------------
 
@@ -11,7 +10,6 @@ function validSearch() {
 
     if ($("#text_box").val() === "") {
         console.log("User did not enter any string. No search is performed.");
-        //$("#results").prepend(empty_string_validation_message);
         $("#myModal").modal();
     }
     else {
