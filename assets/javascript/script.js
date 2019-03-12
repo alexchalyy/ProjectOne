9bdef594536dc7912ac5443d4c55825216518918
ronseide <ron.seide@gmail.com>
2019-03-12 18:59:25 -0400

    JS file can talk to petfinder api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,66 @@
 /*  This script utilizes api get calls to get dog breed information by search result and pull up local pet shelter results. */
 
-var breed;  //  This holds the dog breed name.
-
-//----------------------------------------------------------------------------------------------------------
+var pfApiKey = "3b7e9ed23b598ca17ae1d73381f1544f"; // Petfinder.com API key to be appended to Petfinder.com url
 
 function validSearch() {
-
     //  This is validation function that makes sure that user enters a correct value
-
     if ($("#text_box").val() === "") {
         console.log("User did not enter any string. No search is performed.");
         $("#myModal").modal();
+        return false;
     }
-    else {
-        breed = document.getElementById("text_box").value;
-        console.log("user entered a value of " + breed);
-    }
+    return true;
 }
+$(document).ready(function () {
 
-//----------------------------------------------------------------------------------------------------------
-
-$("#submit_button").on("click",
-
-    function (event) {
-
-        //  This function is called when the user clicks submit button.
-        //  It gets the value of text box.
+    $("#submit_button").on("click", function (event) {
+        //  This needs to become a select box so that the user input is idential to breed.list on the PetFinder.com api. The list is found here: https://api.petfinder.com/breed.list?animal=dog&format=json&key=8ed9687e011d3ec63d28893e2ae6348e.
 
         //  This line prevents refreshing when user clicks on submit button.
-
         event.preventDefault();
+
+        //  This is validation function that makes sure that user enters a correct value
+        if (validSearch() == false) {
+            return;
+        }
+        // var breed = $("#text_box").val(); // I've hardcoded var breed below for testing purposes. This is to be replaced such that var breed is the value (the breed name) from the select box
+        var breed = "Pit Bull Terrier"
+        console.log("Breed is " + breed);
+
+        var pfUrl = "https://api.petfinder.com/pet.find?key=" + pfApiKey + "&location=44216&status=A&breed=" + breed + "&count=5&output=basic&format=json";
+        console.log(pfUrl);
+        $.ajax({
+            url: pfUrl,
+            dataType: 'jsonp',
+            success: function (jsonpData) {
+                console.log(jsonpData);
+                /* Here is where we need to start pulling out things like name and a photo from jsonpData. The below is more pseudocode than anything else. 
+                //jsonpData.petfinder.pets;
+                var dogData = jsonpData.petfinder.pets.pet;
+                console.log(dogData);
+                for(var i=0;i<dogData.length;i++) {
+                    var dogName = dogData[i].name;
+                    console.log(dogName);
+                }
+                */
+
+                //var dogPhoto = dogData.pets.pet.(img of some sort)
+            }
+        });
+
+        /*
+
+
+
 
         //  These lines empty breed description and search results when submit button is clicked before new search is performed.
 
         $("#pet_description").empty();
         $("#results").empty();
 
-        //  This is validation function that makes sure that user enters a correct value
-
-        validSearch();
-
         //  This clears the search box, after user clicks submit button.
 
         $("#text_box").val("");
 
     });
+});
