784d994349fa9e4dec380e96a0e1814545c6c6ea
ronseide <ron.seide@gmail.com>
2019-03-13 18:09:03 -0400

    dogPhoto and dogText all styled. PF API functional to requirements.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,32 +3,31 @@
 $(document).ready(function () {
     $("#submit_button").on("click", function (event) {
         event.preventDefault();
-        var breed = "Pit Bull Terrier"
-        console.log("Breed is " + breed);
+        var breed = "Pit Bull Terrier" // To be replaced with values from the select box.
 
+        //Generate API URLs for Petfinder and Wikipedia.
         var pfApiKey = "3b7e9ed23b598ca17ae1d73381f1544f";
-        var pfUrl = "https://api.petfinder.com/pet.find?key=" + pfApiKey + "&location=44216&status=A&breed=" + breed + "&count=5&output=basic&format=json";
-        // console.log(pfUrl);
+        var pfUrl = "https://api.petfinder.com/pet.find?key=" + pfApiKey + "&location=44113&status=A&breed=" + breed + "&count=5&output=basic&format=json";
         var queryUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=pit_bull";
-
+        
+        // Petfinder API call. 
         $.ajax({
             url: pfUrl,
             dataType: 'jsonp',
             method: "GET"
         }).then(function (response) {
             dogData = response.petfinder.pets.pet;
-            console.log(dogData);
             for (var i = 0; i < 5; i++) {
                 var dogData = response.petfinder.pets.pet[i];
                 var dogPhoto = dogData.media.photos.photo[1].$t;
-                console.log(dogPhoto);
                 $("#dogPhoto"+i).attr("src", dogPhoto);
                 var dogName = dogData.name.$t;
-                console.log(dogName);
-                $("#dogText"+i).html(dogName + "<br></br>Test");
+                var dogLocation = dogData.contact.city.$t;
+                var dogPhone = dogData.contact.phone.$t;
+                $("#dogText"+i).html("<b>" + dogName + "</b><br>Location:<br>" + dogLocation + "<br>Call:<br>" + dogPhone);
             }
         })
-        // bryan b api info
+        // Wikipedia API call.
         var dogBreed = [
             {dog:"pit_bull",
             pagenumber:"64235",
