c6d3b514e9d92eaa3cc898506a9846e782acdbc9
alex chalyy <alexchalyy@gmail.com>
2019-03-12 20:16:08 -0400

    drop down menu added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,9 @@
 
 var pfApiKey = "3b7e9ed23b598ca17ae1d73381f1544f"; // Petfinder.com API key to be appended to Petfinder.com url
 
+/*
+This is no longer necessary since a drop down menu would be used instead.
+
 function validSearch() {
     //  This is validation function that makes sure that user enters a correct value
     if ($("#text_box").val() === "") {
@@ -10,25 +13,40 @@ function validSearch() {
         return false;
     }
     return true;
-}
+}*/
+
 $(document).ready(function () {
 
     $("#submit_button").on("click", function (event) {
         //  This needs to become a select box so that the user input is idential to breed.list on the PetFinder.com api. The list is found here: https://api.petfinder.com/breed.list?animal=dog&format=json&key=8ed9687e011d3ec63d28893e2ae6348e.
 
         //  This line prevents refreshing when user clicks on submit button.
+
         event.preventDefault();
 
         //  This is validation function that makes sure that user enters a correct value
-        if (validSearch() == false) {
+        //  This is no longer necessary since drop down menu would be used.
+        /*if (validSearch() == false) {
             return;
-        }
+        }*/
         // var breed = $("#text_box").val(); // I've hardcoded var breed below for testing purposes. This is to be replaced such that var breed is the value (the breed name) from the select box
         var breed = "Pit Bull Terrier"
         console.log("Breed is " + breed);
 
         var pfUrl = "https://api.petfinder.com/pet.find?key=" + pfApiKey + "&location=44216&status=A&breed=" + breed + "&count=5&output=basic&format=json";
         console.log(pfUrl);
+        var queryUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=pit_bull";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        })
+
+            .then(function (response) {
+                console.log(response);
+                var results = response.query.pages[64235].extract;
+                console.log(results);
+            });
         $.ajax({
             url: pfUrl,
             dataType: 'jsonp',
@@ -48,11 +66,6 @@ $(document).ready(function () {
             }
         });
 
-        
-
-
-
-
         //  These lines empty breed description and search results when submit button is clicked before new search is performed.
 
         $("#pet_description").empty();
@@ -65,16 +78,18 @@ $(document).ready(function () {
     });
 });
 
+/*  This is for future use of retrieving data from wikipedia.
 
 var queryUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=pit_bull";
-    
+
 $.ajax({
-        url: queryUrl,
-        method: "GET"
-        })
-        
-        .then(function(response) {
-            console.log(response);
-            var results = response.query.pages[64235].extract;
-            console.log(results);
-        });
+    url: queryUrl,
+    method: "GET"
+})
+
+    .then(function (response) {
+        console.log(response);
+        var results = response.query.pages[64235].extract;
+        console.log(results);
+    });
+*/
